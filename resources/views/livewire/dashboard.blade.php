198e928182836b4fb6457b38c5ec3ebac57e9557
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2024-12-04 12:36:13 +0600

    server status done

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,30 @@
 <div class="container-xl">
+    <div class="card my-3">
+        <div class="card-body">
+            <div class="row g-3 align-items-center">
+                <div class="col-auto">
+                <span id="indicator" class="status-indicator status-dark status-indicator-animated">
+                  <span class="status-indicator-circle"></span>
+                  <span class="status-indicator-circle"></span>
+                  <span class="status-indicator-circle"></span>
+                </span>
+                </div>
+                <div class="col">
+                    <h2 class="h3">
+                        Soketi Server at: <a href="{{config('soketi.scheme').'://'.config('soketi.host').':'.config('soketi.port')}}"> {{config
+                        ('soketi.scheme').'://'.config('soketi.host').':'.config('soketi.port')}}</a>
+                    </h2>
+                    <div class="text-secondary">
+                        <ul class="list-inline list-inline-dots mb-0">
+                            <li id="status" class="list-inline-item"><span class="text-dark">Checking</span></li>
+                            <li class="list-inline-item">Checked every 3 minutes</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="col-auto ms-auto d-print-none">
         <div class="btn-list">
             <a class="btn btn-primary btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -9,16 +35,24 @@
     </div>
     <div class="row">
         @foreach($apps as $app)
-            <div class="col-md-3 my-2"  wire:key="app-{{ $app->id }}">
-                <a class="card card-link card-link-rotate" >
+            <div class="col-md-3 my-2" wire:key="app-{{ $app->id }}">
+                <a class="card card-link card-link-rotate">
                     <div class="card-header justify-content-between">
                         <h1 class="card-title">
                             <i class="ti ti-id-badge"></i>
                             Name : {{ $app->name }}
                         </h1>
                         <button wire:click="deleteApp({{$app->id}})">
-                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="red"
-                                  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red"
+                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                 class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M4 7l16 0"/>
+                                <path d="M10 11l0 6"/>
+                                <path d="M14 11l0 6"/>
+                                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"/>
+                                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"/>
+                            </svg>
                         </button>
                     </div>
                     <div class="card-body" wire:navigate href="{{route('app',[$app->id])}}" wire:key="app-{{ $app->id }}">
@@ -75,3 +109,35 @@
         </div>
     </div>
 </div>
+
+
+<script>
+    async function checkSoketiServer() {
+        const indicatorElement = document.getElementById('indicator'); // Element to update
+        const statusElement = document.getElementById('status'); // Element to update
+        const soketiUrl = "{{config('soketi.scheme').'://'.config('soketi.host').':'.config('soketi.port')}}"; // Replace with your Soketi server URL
+
+        try {
+            const response = await fetch(soketiUrl, {method: 'GET'}); // Sending a simple HEAD request
+            if (response.ok) {
+                indicatorElement.classList.remove('status-dark');
+                indicatorElement.classList.add('status-green');
+                statusElement.classList.remove('status-dark');
+                statusElement.classList.add('status-green');
+                statusElement.innerText = 'Up';
+
+            } else {
+                throw new Error('Server responded but with an error status');
+            }
+        } catch (error) {
+            indicatorElement.classList.remove('status-dark');
+            indicatorElement.classList.add('status-red');
+            statusElement.classList.remove('status-dark');
+            statusElement.classList.add('status-red');
+            statusElement.innerText = 'Down';
+        }
+    }
+
+    checkSoketiServer();
+    setInterval(checkSoketiServer, 180000);
+</script>
